d895225397b4ce37c4f5ef425797ee60b4a3e665
sneilk <SeleznevN@ya.ru>
2018-06-09 15:25:11 +0300

    Убираем функцию для сокращения кода

--- task1.rb
+++ task1.rb
@@ -1,16 +1,12 @@
-def palindromeCheck(text)
+text = ARGV[0].to_s.downcase
+if text.length > 0 && text.length <= 10000
+  symb = Regexp.new("[ ;:'\"-\\/&@#$%^*()_+=,.!?]")
+  text = text.gsub(symb, '')
   if text == text.reverse
     puts 'YES'
   else
     puts 'NO'
   end
-end
-
-
-text = ARGV[0].to_s.downcase
-if text.length > 0 && text.length <= 10000
-  symb = Regexp.new("[ ;:'\"-\\/&@#$%^*()_+=,.!?]")
-  palindromeCheck(text.gsub(symb, ''))
 else
   puts "Text Error"
   end
