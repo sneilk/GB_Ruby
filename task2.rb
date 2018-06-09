01d73d0ef96cc36b5215cb5363fde6caf1339d47
sneilk <SeleznevN@ya.ru>
2018-06-09 13:47:32 +0300

    Добавлена проверка ввода

--- task2.rb
+++ task2.rb
@@ -1,21 +1,26 @@
 def searchNum(num)
   numAbs = num.abs
-  if numAbs == 1 || num == 0
-    return numAbs
+  if  numAbs == 0
+    return 0
   else
   nums = [0, 1]
   for i in (2..numAbs)
     nums[0], nums[1] = nums[1], nums[0]
     nums[1] = nums[0] + nums[1]
   end
-if num > 0 || num % 2 == 1
-  return nums[1]
-else
-  return nums[1]*(-1)
+  if num > 0 || num % 2 == 1
+    nums[1]
+  else
+    nums[1]*(-1)
+  end
+  end
 end
-  end
-  end
 
 num = ARGV[0].to_i
-puts searchNum(num)
-
+if  num < -10000
+  puts "Please enter number more than or equal -10000"
+elsif num > 10000
+  puts "Please enter number lesser than 10000"
+else
+  puts searchNum(num)
+end
