8d1998bba1551cce7903d3cce5bc441412d30efa
sneilk <Seleznevn@ya.ru>
2018-06-10 17:30:24 +0300

    функция поиска изменена на рекурсию

--- task2.rb
+++ task2.rb
@@ -1,22 +1,15 @@
-def searchNum(num)
-  numAbs = num.abs
-  return 0 if  numAbs == 0
-  nums = [0, 1]
-  for i in (2..numAbs)
-    nums[0], nums[1] = nums[1], nums[0]+ nums[1]
-  end
-  if num > 0 || num % 2 == 1
-    nums[1]
-  else
-    nums[1]*(-1)
-  end
+def fib(num)
+  return 0 if num == 0
+  return 1 if num.abs == 1
+  return fib(num - 1) +fib(num - 2)
 end
-
 num = ARGV[0].to_i
 if  num < -10000
   puts "Please enter number more than or equal -10000"
 elsif num > 10000
   puts "Please enter number lesser than 10000"
+elsif num >=0 || num%2 == 1
+  puts fib(num.abs)
 else
-  puts searchNum(num)
+  puts -1 * fib(num.abs)
 end
