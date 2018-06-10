6f7f7685cb435d4275bd679fd78f59d2c5b1ae12
sneilk <Seleznevn@ya.ru>
2018-06-10 17:00:47 +0300

    Убираем функцию division

--- task3.rb
+++ task3.rb
@@ -1,6 +1,10 @@
 #The function to division string to vowels and consonants letters and numbers
-
-def division(text)
+text = ARGV[0].to_s
+if text.length > 10000
+  puts "Please enter more short text"
+elsif text.length == 0
+  puts "You don't enter text"
+else
   vow = text.scan(/[qeyuiojaQEYUIOJA]+/).join
   cons = text.scan(/[wrtplkhgfdszxcvbnmWRTPLKHGFDSZXCVBNM]+/).join
   nums = text.scan(/[0-9]+/).join
@@ -8,13 +12,3 @@ def division(text)
   cons += " " if cons != ""
   puts (vow + cons +  nums)
 end
-
-
-text = ARGV[0].to_s
-if text.length > 10000
-  puts "Please enter more short text"
-elsif text.length == 0
-  puts "You don't enter text"
-else
-division(text)
-  end
