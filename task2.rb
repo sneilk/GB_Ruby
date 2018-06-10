f5f2f4cee826260022965ffc7447776d2e5b8dc5
sneilk <Seleznevn@ya.ru>
2018-06-10 17:16:15 +0300

    доработанный поиск числа

--- task2.rb
+++ task2.rb
@@ -1,18 +1,14 @@
 def searchNum(num)
   numAbs = num.abs
-  if  numAbs == 0
-    return 0
-  else
+  return 0 if  numAbs == 0
   nums = [0, 1]
   for i in (2..numAbs)
-    nums[0], nums[1] = nums[1], nums[0]
-    nums[1] = nums[0] + nums[1]
+    nums[0], nums[1] = nums[1], nums[0]+ nums[1]
   end
   if num > 0 || num % 2 == 1
     nums[1]
   else
     nums[1]*(-1)
-  end
   end
 end
 
