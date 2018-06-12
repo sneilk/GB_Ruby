5c5732d354f8fe284596aaa60698493346607d03
sneilk <Seleznevn@ya.ru>
2018-06-12 22:20:42 +0300

    реализация поиска по формуле Бине

--- task2.rb
+++ task2.rb
@@ -1,22 +1,10 @@
-def searchNum(num)
-  numAbs = num.abs
-  return 0 if  numAbs == 0
-  nums = [0, 1]
-  for i in (2..numAbs)
-    nums[0], nums[1] = nums[1], nums[0]+ nums[1]
-  end
-  if num > 0 || num % 2 == 1
-    nums[1]
-  else
-    nums[1]*(-1)
-  end
-end
-
 num = ARGV[0].to_i
 if  num < -10000
   puts "Please enter number more than or equal -10000"
 elsif num > 10000
   puts "Please enter number lesser than 10000"
 else
-  puts searchNum(num)
+  y = Math.sqrt(5)
+  res = (((1 + y)/2)**num- ((1 - y)/2)**num)/y
+  puts res.round
 end
